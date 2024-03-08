191905a1bc5fce16c533ec8884b9c4cb4449dca5
BAD-99 <brandon.andrew.davis@gmail.com>
2024-03-08 06:18:19 -0700

    Change hourly and initialization
    Refined dom initialization and return values of IIFE's to help reduce amount of returned values

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,19 @@
 // head/main pic switchable to different day
 
-export function displayWeatherForecast(weatherData) {
-  displayCurrentWeather(weatherData.current);
+export function displayWeatherForecast(processedWeatherData) {
+  displayCurrentWeather(processedWeatherData.current);
+}
+
+function displayCurrentWeather(data) {
+  const section = currentSection;
+  section.high.textContent = data.high;
+  section.low.textContent = data.high;
+  section.temperature.textContent = data.temperature;
+  section.feelsLike.textContent = data.feelsLike;
+  section.location.textContent = data.location;
+  section.condition.textContent = data.condition.text;
+  section.humidity.textContent = data.humidity;
+  section.wind.textContent = data.wind;
 }
 
 const currentSection = (() => {
@@ -18,7 +30,7 @@ const currentSection = (() => {
 
   const bottomBox = document.createElement("div");
   const wordBox = document.createElement("div");
-  const locationName = document.createElement("div");
+  const location = document.createElement("div");
   const condition = document.createElement("div");
   const numBox = document.createElement("div");
   const humidity = document.createElement("div");
@@ -31,7 +43,7 @@ const currentSection = (() => {
 
   highLowBox.append(high, low);
   tempBox.append(temperature, feelsLike);
-  wordBox.append(locationName, condition);
+  wordBox.append(location, condition);
   numBox.append(humidity, wind);
 
   document.body.append(container);
@@ -42,16 +54,12 @@ const currentSection = (() => {
     img,
     temperature,
     feelsLike,
-    locationName,
+    location,
     condition,
     humidity,
     wind,
   };
 })();
-
-function updateCurrentSection(weatherData) {
-  currentSection.temperature.textContent = currentWeather.temp_f + " °F";
-}
 
 //hourly forecast
 
@@ -59,17 +67,31 @@ const hourlySection = (() => {
   const container = document.createElement("div");
   const info = document.createElement("div");
   const ul = document.createElement("ul");
-  const lis = [];
+  const hours = [];
   for (let i = 0; i < 24; i++) {
+    const hour = {};
     const li = document.createElement("li");
-    lis.push(li);
+    const infoBox = document.createElement("div");
+    hour.name = document.createElement("div");
+    hour.img = document.createElement("img");
+    hour.temperature = document.createElement("div");
+    const precipBox = document.createElement("div");
+    hour.precipitationImg = document.createElement("img");
+    hour.percent = document.createElement("div");
+
+    li.append(infoBox, precipBox);
+
+    infoBox.append(hour.name, hour.img, hour.temperature);
+    precipBox.append(hour.precipitationImg, hour.percent);
+
+    ul.append(li);
+    hours.push(hour);
   }
 
-  ul.append(...lis);
   container.append(info, ul);
   document.body.append(container);
 
-  return { container, info, ul, lis };
+  return { container, info, hours };
 })();
 
 //next days, clickable and changes outline focus to that day
@@ -78,8 +100,27 @@ const dailySection = (() => {
   const container = document.createElement("div");
   const days = [];
   for (let i = 0; i < 3; i++) {
-    const day = document.createElement("div");
-    days.push(day);
+    const day = {};
+    const box = document.createElement("div");
+    const infoBox = document.createElement("div");
+    day.name = document.createElement("div");
+    day.img = document.createElement("img");
+    const highLowBox = document.createElement("div");
+    day.high = document.createElement("div");
+    day.low = document.createElement("div");
+    const precipBox = document.createElement("div");
+    day.precipitationImg = document.createElement("img");
+    day.percent = document.createElement("div");
+
+    container.append(box);
+
+    box.append(infoBox, highLowBox, precipBox);
+
+    infoBox.append(day.name, day.img);
+    highLowBox.append(day.high, day.low);
+    precipBox.append(day.precipitationImg, day.percent);
+
+    days.push(box);
   }
 
   container.append(...days);
@@ -87,9 +128,3 @@ const dailySection = (() => {
 
   return { container, days };
 })();
-
-function displayHourlyTemperatures(hourlyData) {
-  for (let i = 0; i < 24; i++) {
-    hourlySection.lis[i].textContent = hourlyData[i].temp_f;
-  }
-}
